98e51eee76b92d7d8ebff47692756e1dacd47a67
Simon Maher <simon.maher@integrify.io>
2019-05-22 13:17:34 +0300

    Added pokedex cards, completed random function to create winning hand

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react"
-import Pokedex from "./Pokedex"
+import Pokegame from "./Pokegame"
 import "./App.css"
 
 class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <Pokedex />
+                <Pokegame />
 
             </div>
         )
